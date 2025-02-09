d173e8e9e141e8343c452b5b6fa1e0af2e5a6172
Gaurav Shukla <gouravshukla337@gmail.com>
2025-02-09 17:43:46 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './App.css'
 import Navbar from './assets/navbar'
 import Hero from './assets/Hero'
 import Bottomnav from './assets/Dock'
+import Catagory from './assets/Catagory'
+import Occasion from './assets/Occasion'
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
       <Bottomnav/>
+      <Catagory/>
+      <Occasion/>
     </>
   )
 }
---
+++ src/assets/Occasion.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+function Occasion(){
+    return(
+        <div className="flex flex-col items-center justify-between">
+               <h1>Shop by Occasion</h1>
+<div className="carousel rounded-box flex items-center justify-around gap-4">
+ 
+  <div className="carousel-item">
+    <img className="rounded-box"
+      src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img className="rounded-box"
+      src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+      alt="Burger" />
+  </div>
+  <div className="carousel-item">
+    <img className="rounded-box"
+      src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+      alt="Burger" />
+  </div>
+
+</div>
+        </div>
+    )
+}
+export default Occasion;
